1882a6eb2aa666986cc49cfad17238b0ad72b1af
Zeynineph <88530308+Zeynineph@users.noreply.github.com>
2021-08-06 18:17:14 +0800

    Update japs.sh

--- japs.sh
+++ japs.sh
@@ -1,14 +1,7 @@
-
 ##Download Script First
-
 wget https://raw.githubusercontent.com/ZeYNine/websocketscripts/main/japs.py
-
 ##Set to run at startup
-
 sudo cp -i /root/japs.py /bin
-
 ##Cron Job
-
 sudo crontab -e
-
 ##Stop na dito haha
